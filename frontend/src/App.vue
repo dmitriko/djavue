969ef44476adb33f77e6431b0617f86bdffc5b11
DmitriKo <dmitrikozhevin@gmail.com>
2021-04-05 10:53:26 +0800

    scafolding frontend with vue2 and bootstrap

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+      <b-navbar type="dark" variant="info">
+          <b-navbar-brand>Djavue</b-navbar-brand>
+      </b-navbar>
+      <LoginForm  v-if="!isLogged" />
+      <div v-if="isLogged">SubmitJob Form</div>
+
+ </div>
+</template>
+
+<script>
+
+import LoginForm from './components/LoginForm.vue'
+
+export default {
+  name: 'Djavule',
+  computed: {
+      isLogged() {
+          return false
+      }
+  },
+  components: {
+      LoginForm
+  }
+}
+</script>
+
---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <b-contaier>
+         <b-form @submit="onSubmit" class="mx-auto" id="login-form">
+            <b-form-group label="Your Name:">
+                <b-form-input v-model="username" required />
+            </b-form-group>
+            <b-form-group label="Password:">
+                <b-form-input v-model="password" required type="password" />
+            </b-form-group>
+            <div class="col-md-12 text-center">
+              <b-button size="lg" type="submit" variant="primary">Login</b-button>
+            </div>
+         </b-form>
+        </b-contaier>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginForm',
+    data() {
+        return {
+            "username": "",
+            "password": ""
+        }
+    },
+    methods: {
+        onSubmit(event) {
+            event.preventDefault()
+            alert(this.username)
+        }
+    }
+}
+</script>
+
+<style scoped >
+
+  #login-form {
+     width: 50%;
+     margin-top: 10px
+ }
+
+</style>
