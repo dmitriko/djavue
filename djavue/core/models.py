8b4b8e60778a23cf38ec8e5e36b3d16b09aa5a29
DmitriKo <dmitrikozhevin@gmail.com>
2021-04-05 19:27:21 +0800

    fix migration issue

--- djavue/core/models.py
+++ djavue/core/models.py
@@ -15,20 +15,15 @@ class JOB_KIND:
             return name in cls.__dict__
         return False
 
-    @classmethod
-    def choices(cls, include_all=True):
-        all_values = [y for x,y in cls.__dict__.items() if not x.startswith('_')]
-        if include_all:
-            return [(x, x) for x in all_values]
-        return [(x, x) for x in all_values if x != cls.all_three]
-
 
 class Job(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    kind = models.CharField(max_length=50, choices=JOB_KIND.choices())
+    kind = models.CharField(max_length=50, choices=[(x, x) for x in [JOB_KIND.original,
+        JOB_KIND.square_original, JOB_KIND.square_small, JOB_KIND.all_three]])
 
 
 class Image(models.Model):
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     img = models.ImageField(upload_to='images')
-    kind = models.CharField(max_length=50, choices=JOB_KIND.choices(include_all=False))
+    kind = models.CharField(max_length=50, choices=[(x, x) for x in [JOB_KIND.original,
+        JOB_KIND.square_original, JOB_KIND.square_small]])
