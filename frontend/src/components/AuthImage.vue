8252d5fade3e90786907767ab97640259376b30d
DmitriKo <dmitrikozhevin@gmail.com>
2021-04-06 11:11:16 +0800

    implement AuthImage component

---
+++ frontend/src/components/AuthImage.vue
@@ -0,0 +1,45 @@
+<template>
+    <b-img :src="image_src_data" fluid />
+</template>
+
+<script>
+
+import axios from 'axios'
+
+const IMAGE_URL = 'http://127.0.0.1:8000/api/image/'
+
+export default {
+    name: 'AuthImage',
+    props: [
+        'pk'
+    ],
+    data() {
+        return {
+            image_src_data:""
+        }
+    },
+    methods: {
+        fetchSrcData() {
+            let url = IMAGE_URL + this.pk + '/'
+            let auth = 'Token ' + this.$store.state.token
+            axios({
+                method: 'get',
+                url: url,
+                headers: {
+                    'Authorization': auth
+                },
+                responseType: 'arraybuffer'
+            }).then(resp => {
+                let mimeType = resp.headers['content-type'].toLowerCase();
+                let imgBase64 = new Buffer(resp.data, 'binary').toString('base64');
+                this.image_src_data = 'data:' + mimeType + ';base64,' + imgBase64;
+            }).catch(error => {
+                console.log(error.response.data)
+                })
+        }
+    },
+    created() {
+        this.fetchSrcData()
+    }
+}
+</script>
