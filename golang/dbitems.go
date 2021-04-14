c468304302e7d267fa9ed7bb7d9befd7aad82363
DmitriKo <dmitrikozhevin@gmail.com>
2021-04-14 12:31:57 +0800

    start to implement user

--- golang/dbitems.go
+++ golang/dbitems.go
@@ -3,6 +3,7 @@ package main
 import "golang.org/x/crypto/bcrypt"
 
 type User struct {
+	ID       string
 	Username string
 	Password string
 	Token    string
@@ -17,6 +18,10 @@ func NewUser(username, password string) (*User, error) {
 	if err != nil {
 		return nil, err
 	}
-	user := &User{Username: username, Password: string(hash), Token: token}
+	id, err := NewULIDNow()
+	if err != nil {
+		return nil, err
+	}
+	user := &User{ID: id, Username: username, Password: string(hash), Token: token}
 	return user, nil
 }
